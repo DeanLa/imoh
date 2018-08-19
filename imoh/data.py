136860fb2c203d32ddf08381b8cc52a975813163
Dean Langsam <deanla@gmail.com>
2018-08-19 17:05:33 +0300

    Process Data

--- imoh/data.py
+++ imoh/data.py
@@ -2,8 +2,11 @@ import logging
 import os
 import time
 from datetime import datetime
+from glob import glob
 
+import pandas as pd
 import requests
+import xlrd
 
 from imoh import config
 
@@ -41,7 +44,7 @@ def file_exists(base_path, force_download=False):
     return os.path.exists(base_path + '.xls') or os.path.exists(base_path + '.xlsx')
 
 
-def download_reports(years=_years, weeks=_weeks):
+def download_reports(years=_years, weeks=_weeks, force_download=False):
     '''Crawls through IMoH website and download all excel files in the given weeks and years'''
     # Create paths for logging files and download loaction
     base_loc = 'http://www.health.gov.il/PublicationsFiles/IWER'
@@ -52,7 +55,7 @@ def download_reports(years=_years, weeks=_weeks):
             # f = open(log_d, 'a')
             logger.info('{year}_{week}'.format(week=week, year=year))
             base_path = './data/weeklies/{year}_{week:02d}'.format(week=week, year=year)
-            if file_exists(base_path):
+            if file_exists(base_path) and not force_download:
                 logger.debug('Exists')
                 continue
             # There are many different options of paths
@@ -84,3 +87,61 @@ def download_reports(years=_years, weeks=_weeks):
                         logger.warning("{year}_{week:02d} FAILED\n".format(week=week, year=year))
                 except Exception as e:
                     logger.exception(e)
+
+
+def remove_bad_lines(df, week=0):
+    ret = df.copy()
+    removal = 'week no|{}|report|weekly|epidemiological|'.format(week)
+    removal += 'for technical reasons|including cases updated|with or without other'
+    idx = ret['Disease'].str.lower().str.contains(removal)
+    return ret[~idx]
+
+
+def process_file(file_path):
+    stub = os.path.split(file_path)[-1]
+    year, week = [int(x) for x in stub.split('.')[0].split('_')]
+    try:
+        df = pd.read_excel(file_path, sheet_name=str(week))
+    except xlrd.biffh.XLRDError:
+        logger.warning('{} is a bad file'.format(file_path))
+        return pd.DataFrame()
+    df = df.loc[df['Unnamed: 0'].notnull(), :].iloc[:, :-2]
+    df['Unnamed: 0'] = df['Unnamed: 0'].astype(str)
+    week_no = df.loc[df['Unnamed: 0'].str.lower().str.contains('week no'), :]
+    names = week_no.iloc[1, :].astype(str)
+    names[-2] = 'Total'
+    names[-1] = 'CumTotal'
+    df.columns = names
+    df = df.rename(columns={'Week No.': 'Disease'})
+    df = remove_bad_lines(df, week)
+    df['year'] = year
+    df['week'] = week
+    return df
+
+
+def clean_data(df):
+    ordered = ['year', 'week', 'Disease', 'Afula', 'Akko', 'Ashqelon', 'Beer Sheva',
+               'HaSharon', 'Hadera', 'Haifa', 'Jerusalem', 'Kinneret', 'Nazareth',
+               'Petach Tiqwa', 'Ramla', 'Rehovot', 'Tel Aviv', 'Zefat', 'IDF', 'Total',
+               'CumTotal']
+    numeric = ordered[3:]
+    ret = df[ordered].copy()
+    for col in numeric:
+        ret[col] = pd.to_numeric(ret[col], errors='coerce').fillna(0).astype(int)
+    return ret
+
+def make_data(backup=False):
+    filenames = glob('./data/weeklies/20*_*.xls*')
+    dfs = []
+    for fn in filenames:
+        dfs.append(process_file(fn))
+        logger.debug(fn)
+    data = pd.concat(dfs)
+    data = data.rename(columns={"Ha'Sharon": "HaSharon"})
+    if backup:
+        try:
+            data.to_pickle(backup)
+        except:
+            pass
+    clean_data(data)
+    return data
--- run.py
+++ run.py
@@ -1,3 +1,13 @@
+import logging
+
+from imoh import *
+
+logger = logging.getLogger(__file__)
+
 if __name__ == '__main__':
-    from imoh import *
-    download_reports(years=list(range(2016,2019)),weeks=list(range(1,54)))
+    process_file('./data/weeklies/2018_01.xlsx')
+    data = make_data(backup='./data/backup.pickle')
+    data[['year', 'week', 'Disease', 'Afula', 'Akko', 'Ashqelon', 'Beer Sheva',
+       'HaSharon', 'Hadera', 'Haifa', 'Jerusalem', 'Kinneret', 'Nazareth',
+       'Petach Tiqwa', 'Ramla', 'Rehovot', 'Tel Aviv', 'Zefat', 'IDF', 'Total',
+       'CumTotal']].to_pickle('./data/data.pickle')
