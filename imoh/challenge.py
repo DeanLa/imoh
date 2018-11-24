5f03206a57261030f080750272634f74620fca58
DeanLa <DeanLa@gmail.com>
2018-11-24 22:36:37 +0200

    challenge solver and io fixes

---
+++ imoh/challenge.py
@@ -0,0 +1,60 @@
+import logging
+from math import cos, pi, floor
+
+import requests
+
+logger = logging.getLogger(__name__)
+# URL = 'http://www.health.gov.il/PublicationsFiles/IWER01_2004.xls'
+
+
+def parse_challenge(page):
+    """
+    Parse a challenge given by mmi and mavat's web servers, forcing us to solve
+    some math stuff and send the result as a header to actually get the page.
+    This logic is pretty much copied from https://github.com/R3dy/jigsaw-rails/blob/master/lib/breakbot.rb
+    """
+    top = page.split('<script>')[1].split('\n')
+    challenge = top[1].split(';')[0].split('=')[1]
+    challenge_id = top[2].split(';')[0].split('=')[1]
+    return {'challenge': challenge, 'challenge_id': challenge_id, 'challenge_result': get_challenge_answer(challenge)}
+
+
+def get_challenge_answer(challenge):
+    """
+    Solve the math part of the challenge and get the result
+    """
+    arr = list(challenge)
+    last_digit = int(arr[-1])
+    arr.sort()
+    min_digit = int(arr[0])
+    subvar1 = (2 * int(arr[2])) + int(arr[1])
+    subvar2 = str(2 * int(arr[2])) + arr[1]
+    power = ((int(arr[0]) * 1) + 2) ** int(arr[1])
+    x = (int(challenge) * 3 + subvar1)
+    y = cos(pi * subvar1)
+    answer = x * y
+    answer -= power
+    answer += (min_digit - last_digit)
+    answer = str(int(floor(answer))) + subvar2
+    return answer
+
+
+def make_request(url):
+    s = requests.Session()
+    r = s.get(url)
+
+    if 'X-AA-Challenge' in r.text:
+        challenge = parse_challenge(r.text)
+        r = s.get(url, headers={
+            'X-AA-Challenge': challenge['challenge'],
+            'X-AA-Challenge-ID': challenge['challenge_id'],
+            'X-AA-Challenge-Result': challenge['challenge_result']
+        })
+        yum = r.cookies
+        if len(yum) == 0:
+            logger.warning('Empty Cookie Jar')
+            r.status_code = 404
+            return r
+        r = s.get(url, cookies=yum)
+
+    return r
